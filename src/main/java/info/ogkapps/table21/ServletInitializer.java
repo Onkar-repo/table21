a10dfbd5ef446ae57e1d8872caa3cae32fda39fe
Onkar-repo <onkarkulak@gmail.com>
2026-08-06 14:19:49 +0530

    Initial Comment Project Created

---
+++ src/main/java/info/ogkapps/table21/ServletInitializer.java
@@ -0,0 +1,13 @@
+package info.ogkapps.table21;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Table21Application.class);
+	}
+
+}
---
+++ src/main/java/info/ogkapps/table21/Table21Application.java
@@ -0,0 +1,13 @@
+package info.ogkapps.table21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Table21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Table21Application.class, args);
+	}
+
+}
