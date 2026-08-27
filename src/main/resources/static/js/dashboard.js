ac9c68c97693875b8173a8d447d1d5857c55584d
Onkar-repo <onkarkulak@gmail.com>
2026-08-27 22:01:38 +0530

    Written Close Table Script

--- src/main/resources/static/js/dashboard.js
+++ src/main/resources/static/js/dashboard.js
@@ -1,3 +1,5 @@
+console.log("hello");
+
 function loadUserName(){
 	const querryString = window.location.search;
 	const querryParams =  new URLSearchParams(querryString);
@@ -6,6 +8,7 @@ function loadUserName(){
 
 
 function closeTable(){
+	console.log("entered in close table method");
 	document.getElementById("dt").innerText="";
 	document.getElementById("num").innerText="";
 	document.getElementById("tab").innerText="";
@@ -15,8 +18,14 @@ function closeTable(){
 	document.getElementById("itmlist").value="Select an item...";
 	document.getElementById("qty").value="";
 	document.getElementById("srno").value="";
-		
-			
+	console.log("b4 deleting");
+	document.getElementById("itemlist").replaceChildren();
+	console.log("after deleted");
+	document.getElementById("total").innerText="";
+	document.getElementById("printButton").hidden = true;
+	document.getElementById("clearButton").hidden = true;
+	document.getElementById("completeButton").hidden = true;
+	document.getElementById("t1").focus();
 }
 
 function toggleModal() {
