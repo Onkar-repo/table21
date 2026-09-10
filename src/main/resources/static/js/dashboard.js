3bfca3e817d194bd2949ea22e7344765cdb9e796
Onkar-repo <onkarkulak@gmail.com>
2026-09-10 14:30:43 +0530

    Loaded User's Registered Items Into Drop Down

--- src/main/resources/static/js/dashboard.js
+++ src/main/resources/static/js/dashboard.js
@@ -1,62 +1,94 @@
 console.log("hello");
-function loadUserName(){
-	const querryString = window.location.search;
-	const querryParams =  new URLSearchParams(querryString);
-	document.getElementById('ue').innerText = querryParams.get('userEmail');
+function loadUserName() {
+    const querryString = window.location.search;
+    const querryParams = new URLSearchParams(querryString);
+    document.getElementById('ue').innerText = querryParams.get('userEmail');
+}
+
+async function loaduserItems() {
+    try {
+		const url = new URL("http://localhost:8080/dashboard/loaditems");
+		url.search = new URLSearchParams({billUser: document.getElementById('ue').innerText }).toString();
+        const itemsListResponse = await fetch(url);
+        if (!itemsListResponse.ok) {
+            console.log(itemsListResponse.status + ": " + itemsListResponse.statusText);
+            // display custom dialog error
+        }
+        else {
+            const listOfItems = await itemsListResponse.json();
+            // retrive items list and populate in combo box
+            if (listOfItems.message === "Requested without authentication.") {
+                // display custom dialog error
+            }
+            else {
+				dropdown = document.getElementById("itmlist");
+				dropdown.innerHTML = "<option selected>Select an item...</option>";
+
+				for(let i=0;i<listOfItems.length;i++){
+				const temp = document.createElement("option");
+					temp.value = listOfItems[i].itemCode + ":" + listOfItems[i].itemName;
+					temp.textContent = listOfItems[i].itemCode + ":" + listOfItems[i].itemName;
+					dropdown.appendChild(temp);					
+				}
+
+            }
+        }
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+
+async function createEmptyBill() {
+
+    try {
+        const newEmptyBillRequest = {
+            billUser: document.getElementById('ue').innerText,
+            billTable: document.getElementById("t1").value,
+            billStatus: "Pending"
+        };
+        const mhb = {
+            method: 'POST',
+            headers: { 'Content-Type': 'Application/json', 'Access-Control-Allow-Origin': '*' },
+            body: JSON.stringify(newEmptyBillRequest)
+        };
+        newEmptyBillResponse = await fetch("http://localhost:8080/dashboard", mhb);
+        if (!newEmptyBillResponse.ok) {
+            console.log(newEmptyBillResponse.status + ": " + newEmptyBillResponse.statusText);
+        }
+        else {
+            jsonResponse = await newEmptyBillResponse.JSON();
+            // retrive table bill and items data and populate on screen	
+
+        }
+
+
+    }
+    catch (err) {
+        console.log(err);
+    }
 }
 
 
-
-async function createEmptyBill(){
-	
-	try{
-	const newEmptyBillRequest = {
-		billUser: document.getElementById('ue').innerText,
-		billTable: document.getElementById("t1").value,
-		billStatus:	"Pending"
-	};
-const mhb = {
-	method : 'POST',
-	headers: { 'Content-Type': 'Application/json', 'Access-Control-Allow-Origin': '*' },
-	body: JSON.stringify(newEmptyBillRequest)
-};
-newEmptyBillResponse = await fetch("http://localhost:8080/dashboard", mhb);
-if(!newEmptyBillResponse.ok){
-	console.log(newEmptyBillResponse.status + ": " + newEmptyBillResponse.statusText);
-}
-else{
-	jsonResponse = await newEmptyBillResponse.JSON();
-// retrive table bill and items data and populate on screen	
-	
-}
-
-
-}
-catch(err){
-	console.log(err);
-}
-}
-
-
-function closeTable(){
-	console.log("entered in close table method");
-	document.getElementById("dt").innerText="";
-	document.getElementById("num").innerText="";
-	document.getElementById("tab").innerText="";
-	document.getElementById("stat").innerText="";
-	document.getElementById("pmt").innerText="";
-	document.getElementById("itmcode").value="";
-	document.getElementById("itmlist").value="Select an item...";
-	document.getElementById("qty").value="";
-	document.getElementById("srno").value="";
-	console.log("b4 deleting");
-	document.getElementById("itemlist").replaceChildren();
-	console.log("after deleted");
-	document.getElementById("total").innerText="";
-	document.getElementById("printButton").hidden = true;
-	document.getElementById("clearButton").hidden = true;
-	document.getElementById("completeButton").hidden = true;
-	document.getElementById("t1").focus();
+function closeTable() {
+    console.log("entered in close table method");
+    document.getElementById("dt").innerText = "";
+    document.getElementById("num").innerText = "";
+    document.getElementById("tab").innerText = "";
+    document.getElementById("stat").innerText = "";
+    document.getElementById("pmt").innerText = "";
+    document.getElementById("itmcode").value = "";
+    document.getElementById("itmlist").value = "Select an item...";
+    document.getElementById("qty").value = "";
+    document.getElementById("srno").value = "";
+    console.log("b4 deleting");
+    document.getElementById("itemlist").replaceChildren();
+    console.log("after deleted");
+    document.getElementById("total").innerText = "";
+    document.getElementById("printButton").hidden = true;
+    document.getElementById("clearButton").hidden = true;
+    document.getElementById("completeButton").hidden = true;
+    document.getElementById("t1").focus();
 }
 
 function toggleModal() {
@@ -66,7 +98,7 @@ function toggleModal() {
 
 function saveItem() {
     const name = document.getElementById('newItemName').value;
-    if(name) {
+    if (name) {
         alert(name + " added!");
         toggleModal();
     }
